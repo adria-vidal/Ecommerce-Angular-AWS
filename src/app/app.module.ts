7c0fa8fe727fd4c2440f4d7131b2d4e37efe4a71
Adria Vidal <adriavidal1998@gmail.com>
2022-12-01 00:51:50 +0100

    navbar + importaciones de modulos/componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ElementsModule } from './elements/elements.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
