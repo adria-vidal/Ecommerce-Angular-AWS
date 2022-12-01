23a8c710869b60040c749feabe8d402ac7c23ea6
Adria Vidal <adriavidal1998@gmail.com>
2022-12-01 13:14:07 +0100

    configuración rutas en app-routing con sus correspondientes importaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ElementsModule } from './elements/elements.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { ElementsModule } from './elements/elements.module';
   ],
   imports: [
     BrowserModule,
-    ElementsModule
+    ElementsModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
